8d9e109b4b2b017edbf1e6ba9b9735ffe675f11c
testtesttesttesttesttestaaaa <103189094+testtesttesttesttesttestaaaa@users.noreply.github.com>
2023-02-21 13:55:15 -0800

    Update download.sh

--- download.sh
+++ download.sh
@@ -2,5 +2,5 @@ wget https://github.com/PHPMailer/PHPMailer/archive/master.zip
 unzip master.zip
 mv PHPMailer-master phpmailer
 cd phpmailer
-rm VERSION SECURITY.md README.md LICENSE get_oauth_token.php composer.json COMMITMENT
+rm VERSION SECURITY.md README.md LICENSE get_oauth_token.php composer.json COMMITMENT master.zip
 rm -r language/
