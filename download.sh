24f9d54849adc92254bdc8c651339cd684d3bc54
testtesttesttesttesttestaaaa <103189094+testtesttesttesttesttestaaaa@users.noreply.github.com>
2023-02-21 06:56:51 -0800

    Add files via upload

---
+++ download.sh
@@ -0,0 +1,6 @@
+wget https://github.com/PHPMailer/PHPMailer/archive/master.zip
+unzip master.zip
+mv PHPMailer-master phpmailer
+cd phpmailer
+rm VERSION SECURITY.md README.md LICENSE get_oauth_token.php composer.json COMMITMENT
+rm -r language/
