496e5a778eacb4f5955f623a006574702e0d5b54
testtesttesttesttesttestaaaa <103189094+testtesttesttesttesttestaaaa@users.noreply.github.com>
2023-02-23 07:13:17 -0800

    Delete download.sh

--- download.sh
+++
@@ -1,6 +0,0 @@
-wget https://github.com/PHPMailer/PHPMailer/archive/master.zip
-unzip master.zip
-mv PHPMailer-master phpmailer
-cd phpmailer
-rm VERSION SECURITY.md README.md LICENSE get_oauth_token.php composer.json COMMITMENT master.zip
-rm -r language/
