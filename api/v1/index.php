3544afd72c0daf3e98bc69c2519e8ac4ce1285be
testtesttesttesttesttestaaaa <103189094+testtesttesttesttesttestaaaa@users.noreply.github.com>
2023-02-25 12:49:03 -0800

    Add files via upload

--- api/v1/index.php
+++ api/v1/index.php
@@ -8,23 +8,80 @@ use Dotenv\Dotenv;
 //Load Composer's autoloader
 require '../../vendor/autoload.php';
  
+
+function message($msg, $http_code){
+    http_response_code($http_code);
+    header('Content-Type: application/json');
+    echo json_encode(array('message' => $msg));
+} 
+
 // load envs
 $dotenv = Dotenv::createImmutable('../../');
 $dotenv->load();
-$EMAIL = $_ENV["email"];
+$EMAIL = $_ENV["EMAIL"];
+
+session_start();
+
+// Set the time limit to 60 seconds
+$time_limit = 60;
+
+// Set the maximum number of requests per time limit
+$max_requests = 5;
+
+// Get the client's IP address
+$client_ip = $_SERVER['REMOTE_ADDR'];
+
+// Load the requests data from file
+$requests_file = 'requests.json';
+if (file_exists($requests_file)) {
+    $requests = json_decode(file_get_contents($requests_file), true);
+} else {
+    $requests = array();
+}
+
+// Check if the IP address has exceeded the limit
+$request_data = isset($requests[$client_ip]) ? $requests[$client_ip] : array('count' => 0, 'last_request' => null);
+$request_count = $request_data['count'];
+$last_request = $request_data['last_request'];
+$now = time();
+
+if ($last_request && $now - $last_request < $time_limit) {
+    if ($request_count >= $max_requests) {
+        message("Too many requests from your IP address. Please try again later.", 429);
+        exit;
+    } 
+    
+    else {
+        $request_count++;
+        $requests[$client_ip]['count'] = $request_count;
+        file_put_contents($requests_file, json_encode($requests));
+    }
+} else {
+    $requests[$client_ip] = array(
+        'count' => 1,
+        'last_request' => $now
+    );
+    file_put_contents($requests_file, json_encode($requests));
+}
 
 if($_SERVER['REQUEST_METHOD'] == 'POST'){
-    
-    if(isset($_POST['title']) && isset($_POST["description"]) && isset($_POST["name"]) ){
+
+   $expected_keys = array('title', 'description','is_private', 'sysinfo');
+
+   if(array_diff($expected_keys, array_keys($_POST)) === array()){
+       
+        
         $title = $_POST['title'];
         $description = $_POST["description"];
         $name = $_POST['name'];
+        $is_private = $_POST['is_private'];
+        $sysinfo = $_POST['sysinfo'];
         
         if(isset($_POST['email']))
             $reporter_email = $_POST['email'];
-
-        // Set the response content type to JSON
-        header('Content-Type: application/json');
+          
+        if(isset($_POST['name']))
+            $name = $_POST['name'];
       
         // Create a new PHPMailer instance
         $mail = new PHPMailer();
@@ -37,50 +94,56 @@ if($_SERVER['REQUEST_METHOD'] == 'POST'){
         $mail->Password = $_ENV['MAIL_PASSWORD'];
         $mail->SMTPSecure = 'tls';
         $mail->Port = 587;
+        $mail->isHtml(true);
+       
         
         // Set the email content
         $mail->setFrom('bug.reporter.bot@outlook.com', 'Bug Reporter Bot');
         $mail->addAddress($EMAIL);
-        $mail->Subject = $title;
-        $mail->Body = $description;
+
+        if(isset($name))
+          $mail->Subject = $name.": ".$title;
+        else
+          $mail->Subject = $title;
+
+        $mail->Body = $description . "<br><br>" .  "-------------------- <br>" . "Email Private Status: " . $is_private . "<br> System Info: ". $sysinfo;
         
         
+   
         // Send the email
         if ($mail->send()) {
-             echo json_encode(array('message' => "E-mail has been sent successfully"));
+            message("E-mail has been sent successfully.", 200);
+            
+                 
         } else {
-                  echo json_encode(array('message' => "An error occurred while sending E-mail"));
+            message("An error occurred while sending E-mail." , 500);
+     
         }
 
         $mail->ClearAddresses();
-        $mail->ClearAttachments();
 
         if(isset($reporter_email)){
             $mail->addAddress($reporter_email);
-            $mail->Subject = 'Thanks ! You have successfully reported the bug';
-            $mail->Body = 'Dear '.$name.', \n You have successfully reported the bug. We will get in touch with you. \n Summand Team,';
-            if ($mail->send()) {
-             echo json_encode(array('message' => "E-mail has been sent successfully"));
+            $mail->Subject = 'Thanks ! You have successfully reported the bug.';
+            $mail->Body = file_get_contents('email-template.html') ;
+          
+            
+            if ($mail->send() == false) {
+               http_response_code(503);
+               message("An error occurred while sending E-mail.", 500);
             } 
-            else {
-                  echo json_encode(array('message' => "An error occurred while sending E-mail"));
-            }
+            
         }
-
 
     }
     
     else{
-         echo json_encode(array('message' => "Error: you have to send required variables as POST request" ));
+         message("Error: you have to send required variables as POST request.", 500);
     }
-     
-
- 
 } 
 
 else{
-    echo json_encode(array('message' => "Invalid Request Method."));
+    message("Invalid Request Method.", 400);
 }
-
 
 ?>
